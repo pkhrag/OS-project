5a7cbb7a99e7452d001cdd6546f6c63cbcf3cb0e
Ubuntu <pkhrag@iitk.ac.in>
2017-09-04 19:14:45 +0000

    yield

--- nachos/code/userprog/exception.cc
+++ nachos/code/userprog/exception.cc
@@ -223,6 +223,13 @@ ExceptionHandler(ExceptionType which)
         machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
         machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
     }
+    else if((which == SyscallException) && (type == SysCall_Yield)){
+        currentThread->YieldCPU();
+        // Advance program counters.
+        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+    }
     else {
 	printf("Unexpected user mode exception %d %d\n", which, type);
 	ASSERT(FALSE);
