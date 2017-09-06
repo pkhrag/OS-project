60934a1f22d0ff01e75ea9c98af43ecbaf05710d
Ubuntu <pkhrag@iitk.ac.in>
2017-09-06 15:54:13 +0000

    num instr

--- nachos/code/userprog/exception.cc
+++ nachos/code/userprog/exception.cc
@@ -230,6 +230,13 @@ ExceptionHandler(ExceptionType which)
         machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
         machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
     }
+    else if((which == SyscallException) && (type == SysCall_NumInstr)){
+        machine->WriteRegister(2, stats->userTicks);
+        // Advance program counters.
+        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+    }
     else {
 	printf("Unexpected user mode exception %d %d\n", which, type);
 	ASSERT(FALSE);
